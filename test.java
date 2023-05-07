67c06338060c8a57e9cda2053f29e3a6162ef411
EricZeller <98104805+EricZeller@users.noreply.github.com>
2023-05-07 12:50:22 +0200

    Create test.java

---
+++ test.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+
+
+public class test {
+
+    public static void main(String ars[])throws IOException {
+        ausgabe();
+    }
+
+    public static void ausgabe(){
+        System.out.println("Hallo");
+    }
+}
